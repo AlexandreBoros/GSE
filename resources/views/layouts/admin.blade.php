8b1b014fa9be54194914cc7fb9fba4f53bbc7e72
alexandreboros <alexandreboros@gmail.com>
2021-03-31 19:27:31 -0300

    publish website

--- resources/views/layouts/admin.blade.php
+++ resources/views/layouts/admin.blade.php
@@ -505,5 +505,27 @@
                 </div>
             </div>
         </div>
+
+        <div class="modal fade" id="relatorio_data_analise" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel2" aria-hidden="true">
+          <div class="modal-dialog" role="document">
+              <div class="modal-content">
+                  <div class="modal-header">
+                      <h5 class="modal-title" id="exampleModalLabel">Relatorio por Data dos processos em Analise</h5>
+                      <button class="close" type="button" data-dismiss="modal" aria-label="Close">
+                          <span aria-hidden="true">×</span>
+                      </button>
+                  </div>
+                  <div class="modal-body">
+                    
+                  </div>
+                  <div class="modal-footer">
+                          <button class="btn btn-secondary" type="button" data-dismiss="modal">Reset</button>
+                          <button class="btn btn-primary alterar-status">Enviar</button>
+                   </div>
+              </div>
+          </div>
+      </div>
+
+
     </body>
 </html>
