d6d991076c940ced1667097f6bb2ecbf2d8cbd06
alexandreboros <alexandreboros@gmail.com>
2021-03-31 22:13:42 -0300

    publish website

--- resources/views/app/admin/analise_pdf.blade.php
+++ resources/views/app/admin/analise_pdf.blade.php
@@ -1,12 +1,62 @@
-@extends('layouts.pdf')
+<!doctype html>
+<html>
+    <head>
+        <meta http-equiv="Content-Type" content="text/html;charset=utf-8" />
+        <title>{{env('APP_NAME')}}</title>
 
-@section('titulo', 'Relatório Processo Analise')
+        <style>
+            body {
+                font-family:sans-serif;
+                font-size:14px;
+                padding:50px;
+            }
+            div#header {
+                margin-bottom:50px;
+                text-align:center;
+            }
+            div#footer {
+                border:0px;
+                position:fixed;
+                width:100%;
+            }
 
-@section('content')
-
-    <div class="table-responsive">
-            <!--<table id="table_admin" class="display" style="width:100%">-->
-            <table id="table_alunos" class="table01" style="width:100%">    
+            table.table01 {
+                border:1px solid #000;
+                border-collapse:collapse;
+            }
+            table.table01 tr>th {
+                vertical-align:top;
+            }
+            table.table01 tr>td {
+                padding:0 8px 8px 8px;
+                vertical-align:top;
+            }
+            table.table01 tr>td+td {
+                border-left:1px solid #000;
+            }
+            table.bt-0 {
+                border-top:0;
+            }
+            .page-break {
+                page-break-after: always;
+            }
+        </style>
+    </head>
+    <body>
+        <div id="header">
+            <table border="0" cellpadding="0" cellspacing="0">
+                <tr>
+                    <td>
+                        <img src="../public/img/logo.png" alt="{{env('APP_NAME')}}" width="150px" />
+                    </td>
+                    <td align="right">
+                        Relatório Processo Analise 
+                    </td>
+                </tr>
+            </table>
+        </div>
+        <div>
+        <table id="table_alunos" class="table01">    
                 <thead>
                     <tr>
                         <th>CLINICA</th>
@@ -63,7 +113,10 @@
                         </tr>
                     @endif
                 </tbody>
-        </table>
-    </div>
+            </table>
+        </div>
+    </body>
+</html>
 
-@stop
+    
+
