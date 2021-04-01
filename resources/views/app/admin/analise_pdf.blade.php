f9cc8c7682e4df3e73d76468e428d753266c9dc4
alexandreboros <alexandreboros@gmail.com>
2021-03-31 22:19:01 -0300

    publish website

--- resources/views/app/admin/analise_pdf.blade.php
+++ resources/views/app/admin/analise_pdf.blade.php
@@ -55,66 +55,60 @@
                 </tr>
             </table>
         </div>
-        <div>
-        <table id="table_alunos" class="table01">    
-                <thead>
-                    <tr>
-                        <th>CLINICA</th>
-                        <th>NOME</th>
-                        <th>CONVENIO</th>
-                        <th>SITUAÇÃO</th>
-                        <th>CPF</th>
-                        <th>SENHA</th>
-                        <th>DATA</th>
-                        <th>PROTOCOLO</th>
-                        <th>VALOR NF</th>
-                        <th>VALOR PAGO</th>
-                        <th>DATA PAGAMENTO</th>
-                        <th>AÇÕES</th>
-                    </tr>
-                </thead>
-                <tbody>               
-                    @if (count($convenios)>0)
-                        <?php $classe = "" ?>
-                        @foreach ($convenios as $convenio)
-                            @if($convenio->status_situacao == 1)
-                                <?php $classe = 'analise' ?>
-                            @elseif($convenio->status_situacao == 2)
-                                <?php $classe = 'pendente' ?>   
-                            @elseif($convenio->status_situacao == 3)
-                                <?php $classe = 'baixado' ?>     
-                            @elseif($convenio->status_situacao == 4)
-                                <?php $classe = 'pago' ?>                                    
-                            @endif 
-                            <tr class="{{$classe}}">  
-                                <td>{{$convenio->nome_clinica}}</td>
-                                <td>{{$convenio->nome_paciente}}</td>
-                                <td>{{$convenio->tipo_convenio}}</td>
-                                <td class="text-uppercase">{{$convenio->nome_processo_status}}</td>
-                                <td class="text-uppercase">{{$convenio->cpf}}</td>
-                                <td class="text-uppercase">{{$convenio->senha}}</td>
-                                <td class="text-uppercase">{{$convenio->dt_cadastro}}</td>
-                                <td class="text-uppercase">{{$convenio->protocolo}}</td>
-                                <td class="text-uppercase">{{$convenio->valor_nf}}</td>
-                                <td class="text-uppercase">{{$convenio->valor_pago}}</td>
-                                <td class="text-uppercase">{{$convenio->dt_pagamento}}</td>
-                            </tr>  
-                        @endforeach
-                    @else
-                        <tr>
-                            <td colspan="12">
-                                <div class="alert alert-danger">
-                                    <i class="glyphicon glyphicon-remove"></i>
-                                    <div class="mensagem">
-                                        Nenhum Processo foi encontrado.
-                                    </div>
-                                </div>
-                            </td>
-                        </tr>
-                    @endif
-                </tbody>
-            </table>
-        </div>
+        <h3 align="center">DADOS DO VEÍCULO</h3>
+        <table class="table" cellpadding="0" cellspacing="0">
+            <tr>
+                <th>CLINICA</th>
+                <th>NOME</th>
+                <th>CONVENIO</th>
+                <th>SITUAÇÃO</th>
+                <th>CPF</th>
+                <th>SENHA</th>
+                <th>DATA</th>
+                <th>PROTOCOLO</th>
+                <th>VALOR NF</th>
+                <th>VALOR PAGO</th>
+                <th>DATA PAGAMENTO</th>
+            </tr>
+            @if (count($convenios)>0)
+            <?php $classe = "" ?>
+            @foreach ($convenios as $convenio)
+                @if($convenio->status_situacao == 1)
+                    <?php $classe = 'analise' ?>
+                @elseif($convenio->status_situacao == 2)
+                    <?php $classe = 'pendente' ?>   
+                @elseif($convenio->status_situacao == 3)
+                    <?php $classe = 'baixado' ?>     
+                @elseif($convenio->status_situacao == 4)
+                    <?php $classe = 'pago' ?>                                    
+                @endif 
+                <tr class="{{$classe}}">  
+                    <td>{{$convenio->nome_clinica}}</td>
+                    <td>{{$convenio->nome_paciente}}</td>
+                    <td>{{$convenio->tipo_convenio}}</td>
+                    <td class="text-uppercase">{{$convenio->nome_processo_status}}</td>
+                    <td class="text-uppercase">{{$convenio->cpf}}</td>
+                    <td class="text-uppercase">{{$convenio->senha}}</td>
+                    <td class="text-uppercase">{{$convenio->dt_cadastro}}</td>
+                    <td class="text-uppercase">{{$convenio->protocolo}}</td>
+                    <td class="text-uppercase">{{$convenio->valor_nf}}</td>
+                    <td class="text-uppercase">{{$convenio->valor_pago}}</td>
+                    <td class="text-uppercase">{{$convenio->dt_pagamento}}</td>
+                </tr>  
+            @endforeach
+        @else
+            <tr>
+                <td colspan="11">
+                    <div class="alert alert-danger">
+                        <i class="glyphicon glyphicon-remove"></i>
+                        <div class="mensagem">
+                            Nenhum Processo foi encontrado.
+                        </div>
+                    </div>
+                </td>
+            </tr>
+        @endif
+        </table>    
     </body>
 </html>
 
