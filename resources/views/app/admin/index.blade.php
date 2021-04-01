92c7566c33a7b45476d81de838d8d2b448c691df
alexandreboros <alexandreboros@gmail.com>
2021-04-01 09:52:22 -0300

    publish website

--- resources/views/app/admin/index.blade.php
+++ resources/views/app/admin/index.blade.php
@@ -25,15 +25,15 @@
 <div class="row">
     <div class="col-xl-3 col-md-6 mb-4">
         <div class="card border-left-secondary shadow h-100 py-2">
-        '    <div class="card-body">
-                <div class="row no-gutters align-items-center">
-                    <div class="col mr-2">
+         <div class="card-body">
+            <div class="row no-gutters align-items-center">
+                <div class="col mr-2">
                         <div class="text-xs font-weight-bold text-warning text-uppercase mb-1">Analise</div>
                         <div class="h5 mb-0 font-weight-bold text-gray-100 btn btn-secondary btn-sm">
                             {{$procesos_analise->count()}}
                         </div>
-                    </div>
-                    <div class="col-auto">
+                </div>
+                <div class="col-auto">
                         <a href="#" class="relatorio_data_analise" data-toggle="modal" data-target="#relatorio_data_analise" href="javascript:void(0);" alt="Relatorio por Data dos processos em Analise" title="Relatorio por Data dos processos em Analise">
                             <div class="btn btn-secondary btn-sm"  style="margin-top: 22px">
                                 Relatório
@@ -43,9 +43,9 @@
                             <button class="btn btn-primary upload-arquivo">Relatorio</button>
                         </a>
                         <i class="fas fa-comments fa-2x text-gray-300">Relatório</i>--}}
-                    </div>
                 </div>
-            </div>'
+            </div>
+            </div>
         </div>
     </div>
 
