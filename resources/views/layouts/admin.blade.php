3daab9e1c1b59b6d0ab7904ab528888c2359fbe1
alexandreboros <alexandreboros@gmail.com>
2021-04-03 10:12:39 -0300

    publish website

--- resources/views/layouts/admin.blade.php
+++ resources/views/layouts/admin.blade.php
@@ -593,17 +593,18 @@
                 </form>
             </div>
         </div>
+      </div>  
 
       <div class="modal fade" id="relatorio_data_baixado" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel2" aria-hidden="true">
         <div class="modal-dialog" role="document">
             <div class="modal-content">
-                <div class="modal-header">
+              <div class="modal-header">
                     <h5 class="modal-title" id="exampleModalLabel">Relatorio por Data dos processos Baixados</h5>
                     <button class="close" type="button" data-dismiss="modal" aria-label="Close">
                         <span aria-hidden="true">×</span>
                     </button>
-                </div>
-                <form class="user" method="POST" action="{{route("app.generate-pdf")}}">
+              </div>
+              <form class="user" method="POST" action="{{route("app.generate-pdf")}}">
                   <input type="hidden" name="_token" id="token" value="{{ csrf_token() }}">
                   <input type="hidden" name="tipo"  value="3">
                   <div class="modal-body">
@@ -623,9 +624,10 @@
                   <div class="modal-footer">
                     <button class="btn btn-primary alterar-status">Enviar</button>
                   </div>
-                </form>
-            </div>
+              </form>
+          </div>
         </div>
+      </div>  
 
 
         
