692c118b5ab76f4532bbb27888cdc58e03381263
alexandreboros <alexandreboros@gmail.com>
2021-04-14 14:55:50 -0300

    publish website

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -134,7 +134,7 @@ class HomeController extends Controller
                                       
 
                     if($request->seacrh_nome){
-                        $convenios = $convenios->orWhere('nome_paciente', $request->seacrh_nome);
+                        $convenios = $convenios->where('nome_paciente', 'like', '%'.$request->seacrh_nome.'%');
                         dd($convenios->get());
 
                     }                       
