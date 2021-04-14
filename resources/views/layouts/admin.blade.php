0d67e59ad4ae43c941e70cfd76659569da6bcd98
alexandreboros <alexandreboros@gmail.com>
2021-04-14 17:01:10 -0300

    publish website

--- resources/views/layouts/admin.blade.php
+++ resources/views/layouts/admin.blade.php
@@ -211,6 +211,16 @@
             </div>
           </form>
 
+
+          <form action="{{route("home")}}" method="POST" class="d-none d-sm-inline-block form-inline mr-auto ml-md-1 my-2 my-md-0 mw-100 navbar-search">
+            <div class="input-group">
+                <div class="input-group-append">
+                  <button type="submit" class="btn btn-primary" type="button">
+                    <i class="fas fa-search fa-sm">Reset</i>
+                  </button>
+                </div>
+            </div>
+          </form>
           <!-- Topbar Navbar -->
           <ul class="navbar-nav ml-auto">
 
