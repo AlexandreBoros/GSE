ce6080c9dc2166207f6feaa4daa9ec87a3201d0c
alexandreboros <alexandreboros@gmail.com>
2021-04-14 16:27:08 -0300

    publish website

--- routes/web.php
+++ routes/web.php
@@ -19,7 +19,7 @@ Route::get('/', function () {
 
 Auth::routes();
 
-Route::get('/home', 'HomeController@index')->name('home');
+Route::match(['get', 'post'],'/home', 'HomeController@index')->name('home');
 
 // Rotas do sistema com o prefixo "app"
 Route::prefix('app')->group(function ()
