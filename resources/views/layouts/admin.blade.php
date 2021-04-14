66980f087f6f794fdbf1776a733afc0963239b47
alexandreboros <alexandreboros@gmail.com>
2021-04-14 16:34:53 -0300

    publish website

--- resources/views/layouts/admin.blade.php
+++ resources/views/layouts/admin.blade.php
@@ -162,7 +162,21 @@
           <form action="{{route("home")}}" method="POST" class="d-none d-sm-inline-block form-inline mr-auto ml-md-3 my-2 my-md-0 mw-100 navbar-search">
             <div class="input-group">
                 <input type="hidden" name="_token" id="token" value="{{ csrf_token() }}">
-                <input type="text" name="seacrh_nome" class="form-control bg-light border-0 small" placeholder="Pesquisar por nome..." aria-label="Search" aria-describedby="basic-addon2">
+                <label>Filtrar por nome:</label>
+                <input type="text" name="seacrh_nome" class="form-control bg-light border-0 small" placeholder="Pesquisar por nome..." aria-label="Search" aria-describedby="basic-addon2" required="true">
+                <div class="input-group-append">
+                  <button type="submit" class="btn btn-primary" type="button">
+                    <i class="fas fa-search fa-sm"></i>
+                  </button>
+                </div>
+            </div>
+          </form>
+
+          <form action="{{route("home")}}" method="POST" class="d-none d-sm-inline-block form-inline mr-auto ml-md-3 my-2 my-md-0 mw-100 navbar-search">
+            <div class="input-group">
+                <label>Filtrar Clínica:</label>
+                <input type="hidden" name="_token" id="token" value="{{ csrf_token() }}">
+                <input type="text" name="seacrh_nome" class="form-control bg-light border-0 small" placeholder="Pesquisar por nome..." aria-label="Search" aria-describedby="basic-addon2" required="true">
                 <div class="input-group-append">
                   <button type="submit" class="btn btn-primary" type="button">
                     <i class="fas fa-search fa-sm"></i>
