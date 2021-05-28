ae8993ae922d7e3f9bb5e67aac349673f19cde6f
Alexandre Araújo <alexandre.araujo@valid.com>
2021-05-28 15:23:10 -0300

    modificações

--- resources/views/layouts/admin.blade.php
+++ resources/views/layouts/admin.blade.php
@@ -404,7 +404,7 @@
 
         <script async>
           $(document).ready(function() {
-            $("#cpf").mask("000.000.00-00"); 
+            $("#cpf").mask("000.000.000-00"); 
             $("#fone1").mask("(00)00000-0000");
 
             $('#valor_nf').maskMoney({prefix:'R$ ', allowNegative: true, thousands:'.', decimal:',', affixesStay: false});
