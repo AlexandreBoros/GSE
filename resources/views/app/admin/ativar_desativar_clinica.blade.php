affa8ef9d385c5067214c192cd3ddda9c71d87da
Alexandre Araújo <alexandre.araujo@valid.com>
2021-08-02 10:59:06 -0300

    Listar Clinicas Admin

--- resources/views/app/admin/ativar_desativar_clinica.blade.php
+++ resources/views/app/admin/ativar_desativar_clinica.blade.php
@@ -1,3 +1,3 @@
 <input type="hidden" id="id_clinica_ativar_desativar" value="{{$request->id_clinica}}">
-<input type="hidden" id="ativar_desativar" value="{{$request->ativar_desativar}}">
+<input type="hidden" id="ativar_desativar" value="{{$ativar_desativar}}">
 {!! $corpo !!}
