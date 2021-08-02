a0e3f4b0e43b11795027913599c6b5782c74ec10
Alexandre Araújo <alexandre.araujo@valid.com>
2021-08-02 11:36:57 -0300

    Listar Clinicas Admin

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -36,7 +36,7 @@ class HomeController extends Controller
 
             $user = Auth::user();
 
-            $usuario = $users_clinicas->where('id_user', $user->id);
+            $usuario = $users_clinicas->where('id_user', $user->id)->firts();
 
             dd($usuario);
 
