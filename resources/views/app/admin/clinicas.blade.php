f46c76e66e906abece1e26b992bd860eba115bc4
Alexandre Araújo <alexandre.araujo@valid.com>
2021-08-02 09:03:43 -0300

    Listar Clinicas Admin

--- resources/views/app/admin/clinicas.blade.php
+++ resources/views/app/admin/clinicas.blade.php
@@ -91,7 +91,7 @@
     </div>
 </div>--}}
 
-{{dd($clinicas)}}
+{{--dd($clinicas)--}}
 
 <div class="card shadow mb-4"><div class="card-header py-3">
     <h6 class="m-0 font-weight-bold text-primary">Clínicas</h6>
