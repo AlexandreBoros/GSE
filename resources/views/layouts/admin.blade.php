3b7ce079b826f6626fc8467e6ae124e6442ad888
Alexandre Araújo <alexandre.araujo@valid.com>
2022-08-09 17:52:50 -0300

    Novos Layout

--- resources/views/layouts/admin.blade.php
+++ resources/views/layouts/admin.blade.php
@@ -186,6 +186,13 @@
                 <span class="text">Clinicas</span>
             </a>
 
+            <a class="collapse-item"  data-toggle="modal" data-target="#usuario_clinica" href="javascript:void(0);" alt="Nova Usuario" title="Nova Usuario">
+                <span class="icon text-white-50">
+                <i class="fas fa-flag"></i>
+                </span>
+                <span class="text">Novo Usuario</span>
+            </a>
+
         </nav>
         <!-- Topbar -->
         <nav class="navbar navbar-expand navbar-light bg-white topbar mb-4 static-top shadow">
